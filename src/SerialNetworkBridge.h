c7ee4b73406d6d4ff8f1390a5cfa874a2b92ffd5
Suwatchai K. <k_suwatchai@hotmail.com>
2025-11-23 08:53:44 +0700

    Add main header

---
+++ src/SerialNetworkBridge.h
@@ -0,0 +1,10 @@
+#ifndef SERIAL_NETWORK_BRIDGE_H
+#define SERIAL_NETWORK_BRIDGE_H
+
+#include <Arduino.h>
+#include "SerialTCPClient.h"
+#include "SerialWebsocketClient.h"
+#include "SerialUDPClient.h"
+#include "SerialHostManager.h"
+
+#endif
